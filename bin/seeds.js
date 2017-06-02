09ba49c839de89aa90dd89e2e7c9645de4f59a75
Ryan Jones <ryanjones@Ryans-MacBook.local>
2017-06-02 09:18:12 +0200

    corrected bin db

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Country = require('../models/country');
 
-mongoose.connect("mongodb://localhost:27017/country-list");
+mongoose.connect(process.env.MONGODB_URI);
 
 let countries = [
   {
